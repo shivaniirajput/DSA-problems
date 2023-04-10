f7b31f743588991d40b2341827fc171c357ccc08
Shivani <Shivanirajpoot998@gmail.com>
2023-04-10 21:19:55 +0530

    ready to do some changes

---
+++ problemSwitch.cpp
@@ -0,0 +1,13 @@
+//find how many number of notes(100/50/20/1) we are needed in 1330 rupees
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int num = 1330;
+
+  switch(nujm)
+  {
+    case
+  }
+}
---
+++ switch.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int num = 2;
+
+  cout<< endl;
+
+  switch(num)
+  {
+    case 1 : cout<<"first"<<endl;
+    break;
+
+    case 2 : cout<<"second"<<endl;
+    break;
+
+    case 3 : cout<<"third"<<endl;
+    break;
+
+    default : cout<<"this is default case";
+
+
+  }
+
+  return 0;
+}
